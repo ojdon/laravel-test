e59b5e8a9f7e9748f274481a1e2a2374e088fbb6
Ollie Reardon <ojreardon@gmail.com>
2024-02-25 15:33:30 +0000

    Create a sale factory
    For testing Sale data

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sale>
+ */
+class SaleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'unit_cost' => $this->faker->randomFloat(2, 1, 100),
+            'selling_price' => $this->faker->randomFloat(2, 1, 200),
+        ];
+    }
+}
