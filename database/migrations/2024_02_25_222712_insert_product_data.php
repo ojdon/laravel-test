b0a8484642df7de6f71098ff3b1bbf9a3d9bcc6d
Ollie Reardon <ojreardon@gmail.com>
2024-02-25 22:30:37 +0000

    Added Product model and migration

---
+++ database/migrations/2024_02_25_222712_insert_product_data.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        // Insert product data
+        DB::table('products')->insert([
+            ['name' => 'Gold Coffee'],
+            ['name' => 'Arabic Coffee'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
